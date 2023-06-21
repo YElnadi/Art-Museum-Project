ee93659310ff891894094427e2317b3a62f7160d
Yasmine Elnadi <yasmine_adel@hotmail.com>
2023-06-21 10:25:56 -0700

    Art Museum p3 done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import harvardArt from "./data/harvardArt";
 import GalleryNavigation from "./components/GalleryNavigation";
 import { Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
 import GalleryView from "./components/GalleryView";
 
 function App() {
   return (
     <div className="page-wrapper">
+      <Switch>
+        <Route exact path="/">
+          <h2>Harvard Art Museum</h2>
+          <p>
+            Look, but Don't Touch. Please select a Gallery in the navigation
+            bar.
+          </p>
+        </Route>
+
+        <Route exact path="/galleries/:galleryId">
+          <GalleryView galleries={harvardArt.records} />
+        </Route>
+
+        <Route>
+          <h2>Page Not Found</h2>
+        </Route>
+      </Switch>
       <GalleryNavigation galleries={harvardArt.records} />
-      <Route exact path="/galleries/:galleryId">
-        <GalleryView galleries={harvardArt.records}/>
-      </Route>
+
     </div>
   );
 }
